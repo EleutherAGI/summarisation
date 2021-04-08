d65c8b34ab1a0fd205b9496b7a8139e00c100a83
kip <kipgparker@gmail.com>
2021-04-08 18:31:16 +0100

    updating pre-training code and added data prep script

--- pretrain.py
+++ pretrain.py
@@ -2,19 +2,73 @@ import torch
 import transformers
 import argparse
 from utils import TLDRDataset
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from tqdm import tqdm
+import time
+
+def train(args, model, tokenizer, device, train_loader, optimizer, epoch):
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    start_time = time.time()
+
+    model.train()
+    model.zero_grad()
+
+    pbar = tqdm(train_loader)
+
+    for batch_idx, (text, summary_length) in enumerate(pbar):
+
+        inputs = tokenizer(text, padding=True, truncation=True, return_length = True, max_length = 512, return_tensors = 'pt').to(device)
+        total_length = inputs.pop('length')
+
+        lm_logits = model(**inputs)['logits']
+        # Shift so that tokens < n predict n
+        shift_logits = lm_logits[..., :-1, :].contiguous()
+        shift_labels = inputs['input_ids'][..., 1:].contiguous()
+        # Create mask for only end tokens
+        mask = torch.zeros_like(shift_labels, dtype=torch.bool)
+        for i, (s, t) in enumerate(zip(summary_length, total_length)):
+                mask[i][t - s - 1 : t - 1] = True 
+        # Flatten the tokens
+        shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+        shift_labels = shift_labels.view(-1)
+        mask = mask.view(-1)
+        # Calculate loss for summary only
+        loss = criterion(shift_logits[mask], shift_labels[mask])
+        loss /= args.accumulation_steps
+        loss.backward()
+        # Gradient accumulation logic
+        if (i+1) % args.accumulation_steps == 0: 
+            optimizer.step()
+            model.zero_grad()     
+            pbar.set_description(f'train loss: {loss.detach().cpu().item()}')
+
+            if args.dry_run:
+                break
+
+            if (i+1) % evaluation_steps == 0:           # Evaluate the model when we...
+                test            
+
+def test(args, model, tokenizer, device, test_loader, epoch):
+    model.eval()
+    pass
 
 def main():
 
     #Training settings
     parser = argparse.ArgumentParser(description='Fine-Tune causal language model')
 
-    parser.add_argument('--model', type=int, default='gpt2',
+    parser.add_argument('--model', type=str, default='distilgpt2',
                         help='model type to train (default: gpt2)')
 
-    parser.add_argument('--data', type=int, default='data/tldr-filtered.json',
+    parser.add_argument('--data', type=str, default='data/',
                         help='storage location of tldr data')
 
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--accumulation-steps', type=int, default=1,
+                        help='number of gradient accumulation steps')
+
+    parser.add_argument('--batch-size', type=int, default=4, metavar='N',
                         help='input batch size for training (default: 64)')
 
     parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
@@ -46,6 +100,10 @@ def main():
 
     torch.manual_seed(args.seed)
 
+    tokenizer = GPT2Tokenizer.from_pretrained(args.model)
+    tokenizer.pad_token = tokenizer.eos_token
+
+
     device = torch.device("cuda" if use_cuda else "cpu")
 
     train_kwargs = {'batch_size': args.batch_size}
@@ -58,14 +116,22 @@ def main():
         test_kwargs.update(cuda_kwargs)
 
     #TODO add test dataset
-    dataset1 = TLDRDataset(args.data)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset1, **test_kwargs)
+    train_dataset = TLDRDataset(args.data + 'tldr-filtered-train.json', tokenizer)
+    test_dataset = TLDRDataset(args.data + 'tldr-filtered-test.json', tokenizer)
+    train_loader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
 
     model = transformers.GPT2LMHeadModel.from_pretrained(args.model).to(device)
 
-    tokenizer = GPT2Tokenizer.from_pretrained(args.model)
-    tokenizer.pad_token = tokenizer.eos_token
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, tokenizer, device, train_loader, optimizer, epoch)
+        test(args, model, tokenizer, device, test_loader, epoch)
+
+        if args.dry_run:
+            print('finished dry run')
+            break
 
 
 
--- utils/dataset.py
+++ utils/dataset.py
@@ -5,28 +5,31 @@ from torch.utils.data import Dataset
 class TLDRDataset(Dataset):
     """TLDR dataset"""
 
-    def __init__(self, file):
+    def __init__(self, file, tokenizer):
         """
         Args: 
             file (string): Path to the csv file with annotations.
+            tokenizer (transformers.tokenizer): Tokenizer to use for training to get length of encoded data.
         """
+        self.tokenizer = tokenizer
         with open(file) as f:
             self.data = json.load(f)
-
+        
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         """
         Returns: 
-            prompt (string): The prompt portions of the reddit post.
-            summary (string): The summary portions of the reddit post.
+            combined (string): The entire body of the reddit post.
+            summary length (int): The length of summary portion of the reddit post after tokenization.
         """
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         data = self.data[idx]
 
-        return data['content'], data['summary']
+        #Seems like a little bit of a hack to get length of summary, but alright
+        return data['content'] + ' TLDR:' + data['summary'], self.tokenizer(data['summary'], return_length = True)['length']
 
         
