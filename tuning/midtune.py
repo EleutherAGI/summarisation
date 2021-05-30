bab84ff23e8dc5f862764975ac28834fbbc0e734
kip <kipgparker@gmail.com>
2021-05-30 02:20:37 +0100

    simplifying alot of the existing code

---
+++ tuning/midtune.py
@@ -0,0 +1,110 @@
+import torch
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer
+)
+from datasets import load_dataset
+
+MODEL = "distilgpt2"
+
+#load model
+model = AutoModelForCausalLM.from_pretrained(MODEL)
+
+#load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+tokenizer.pad_token = tokenizer.eos_token
+
+# load dataset
+datasets = load_dataset("json", field='data', data_files={
+    "train": "../data/tldr-filtered-test.json",
+    "validation": "../data/tldr-filtered-test.json"
+})
+
+# prep dataset
+def tokenize_function(examples):
+    text = [content + ' TLDR:' + summary for content, summary in zip(examples['content'], examples['summary'])]
+    output = tokenizer(text, return_length = True)
+    output["total_length"] = output.pop("length")
+    output["summary_length"] = tokenizer(examples['summary'], return_length = True)['length']
+    return output
+
+tokenized_datasets = datasets.map(
+    tokenize_function,
+    batched=True,
+    num_proc=1,
+    remove_columns = datasets["train"].column_names
+)
+
+tokenized_datasets['train'][0]
+
+# collate data
+class DataCollatorWithPaddingAndMask:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.padding = True
+        self.max_length = None
+        self.pad_to_multiple_of = None
+
+    def __call__(self, features):
+        #TODO The following columns in the evaluation set  don't have a corresponding argument in `GPT2LMHeadModel.forward` and have been ignored: summary_length, length.
+        total_length = [feature.pop('total_length') for feature in features]
+        summary_length = [feature.pop('summary_length') for feature in features]
+
+        batch = self.tokenizer.pad(
+            features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        mask = torch.full_like(batch['attention_mask'], False)
+        for idx, (t, s) in enumerate(zip(total_length, summary_length)):
+            mask[idx][t-s:t] = True
+        batch['mask'] = mask
+
+        if "label" in batch:
+            batch["labels"] = batch["label"]
+            del batch["label"]
+        if "label_ids" in batch:
+            batch["labels"] = batch["label_ids"]
+            del batch["label_ids"]
+        return batch
+
+collator = DataCollatorWithPaddingAndMask(
+    tokenizer=tokenizer
+)
+
+# define trainer
+training_args = TrainingArguments("test_trainer")
+training_args.remove_unused_columns = False
+
+class MaskedTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        mask = inputs.pop("mask").bool()
+        outputs = model(**inputs)
+        logits = outputs.logits
+        loss_fct = torch.nn.CrossEntropyLoss()
+
+        shift_logits = logits[..., :-1, :].contiguous().view(-1, logits.size(-1))
+        shift_labels = inputs['input_ids'][..., 1:].contiguous().view(-1)
+        shift_mask = mask[..., 1:].contiguous().view(-1)
+
+        loss = loss_fct(shift_logits[shift_mask], shift_labels[shift_mask])
+
+        return (loss, outputs) if return_outputs else loss
+
+
+trainer = MaskedTrainer(
+    model=model,
+    args = training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    tokenizer=tokenizer,
+    # Data collator will default to DataCollatorWithPadding, so we change it.
+    data_collator=collator
+)
+
+trainer.train()
---
+++ tuning/train_reward.py
@@ -0,0 +1,107 @@
+import torch
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer
+)
+from datasets import load_dataset
+
+MODEL = "distilgpt2"
+
+
+#load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+tokenizer.pad_token = tokenizer.eos_token
+
+#load model
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+model.config.pad_token_id = tokenizer.pad_token_id
+
+
+# load dataset
+datasets = load_dataset("json", field='data', data_files={
+    "train": "../data/comparisons-test.json",
+    "validation": "../data/comparisons-test.json"
+})
+
+# prep dataset
+def tokenize_function(examples):
+    output = {}
+    for i in range(2):
+        text = [content['post'] + ' TLDR:' + summary[i]['text'] for content, summary in zip(examples['info'], examples['summaries'])]
+        tokenizer_output = tokenizer(text, return_length = True)
+        output[f"input_ids_{i}"] = tokenizer_output.pop("input_ids")
+        output[f"attention_mask_{i}"] = tokenizer_output.pop("attention_mask")
+    output['label'] = examples['choice']
+    return output
+
+tokenized_datasets = datasets.map(
+    tokenize_function,
+    batched=True,
+    num_proc=1,
+    remove_columns = datasets["train"].column_names
+)
+
+tokenized_datasets['train'][0]
+
+# collate data
+class DualSampleDataCollator:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.padding = True
+        self.max_length = None
+        self.pad_to_multiple_of = None
+
+    def __call__(self, features):
+        batch = {'label': [feature.pop('label') for feature in features]}
+        for i in range(2):
+            needing_padding = {f'input_ids': [feature.pop(f'input_ids_{i}') for feature in features],
+                               f'attention_mask': [feature.pop(f'attention_mask_{i}') for feature in features]}
+
+            output = self.tokenizer.pad(
+                needing_padding,
+                padding=self.padding,
+                max_length=self.max_length,
+                pad_to_multiple_of=self.pad_to_multiple_of,
+                return_tensors="pt",
+            )
+
+            batch[f'input_ids_{i}'] = output[f'input_ids']
+            batch[f'attention_mask_{i}'] = output[f'attention_mask']
+        return batch
+
+collator = DualSampleDataCollator(
+    tokenizer=tokenizer
+)
+
+# define trainer
+training_args = TrainingArguments("test_trainer")
+training_args.remove_unused_columns = False
+
+class ClassificationTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("label")
+
+        outputs_0 = model(input_ids=inputs['input_ids_0'], attention_mask=inputs['attention_mask_0'])
+        outputs_1 = model(input_ids=inputs['input_ids_1'], attention_mask=inputs['attention_mask_1'])
+
+        loss_fct = torch.nn.CrossEntropyLoss()
+        loss = loss_fct(logits_0.view(-1, self.model.config.num_labels),
+                        labels.float().view(-1, self.model.config.num_labels))
+
+        #TODO update loss function here
+
+        return (loss, outputs_0) if return_outputs else loss
+
+trainer = ClassificationTrainer(
+    model=model,
+    args = training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    tokenizer=tokenizer,
+    # Data collator will default to DataCollatorWithPadding, so we change it.
+    data_collator=collator
+)
+
+trainer.train()
